d7cd6b331a062b6ee4a262facd0c5991111e0b98
Roman Dovgan <romadovg@gmail.com>
2024-07-19 11:46:02 +0300

    Enabled tests

--- robot-name/src/test/java/RobotTest.java
+++ robot-name/src/test/java/RobotTest.java
@@ -4,7 +4,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class RobotTest {
