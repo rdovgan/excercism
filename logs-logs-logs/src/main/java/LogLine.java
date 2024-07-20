d03fc377dd9667039b886a6f084336f1d800f36a
Roman Dovgan <romadovg@gmail.com>
2024-07-20 17:47:13 +0300

    Solved simple logs tasks

--- logs-logs-logs/src/main/java/LogLine.java
+++ logs-logs-logs/src/main/java/LogLine.java
@@ -1,13 +1,31 @@
 public class LogLine {
 
+	private final String logLine;
+
 	public LogLine(String logLine) {
+		this.logLine = logLine;
 	}
 
 	public LogLevel getLogLevel() {
-		throw new UnsupportedOperationException("Please implement the getLogLevel() method");
+		if (logLine == null) {
+			return LogLevel.UNKNOWN;
+		}
+		int index = logLine.indexOf("]");
+		if (index == -1) {
+			return LogLevel.UNKNOWN;
+		}
+		String logName = logLine.substring(1, index);
+		return LogLevel.fromName(logName);
 	}
 
 	public String getOutputForShortLog() {
-		throw new UnsupportedOperationException("Please implement the getOutputForShortLog() method");
+		if (logLine == null) {
+			return null;
+		}
+		int index = logLine.indexOf(":");
+		if (index == -1) {
+			return null;
+		}
+		return getLogLevel().getLevel() + ":" + logLine.substring(index + 1).trim();
 	}
 }
