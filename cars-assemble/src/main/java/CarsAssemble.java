d94e19cc0930def8f2561bcfed6ca37c8c90f3fe
Roman Dovgan <romadovg@gmail.com>
2024-07-20 12:27:10 +0300

    Solved learning tasks

--- cars-assemble/src/main/java/CarsAssemble.java
+++ cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,29 @@
 public class CarsAssemble {
 
+	private static final int PRODUCTION_SPEED = 221;
+
+
 	public double productionRatePerHour(int speed) {
-		throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRatePerHour() method");
+		return PRODUCTION_SPEED * defineSuccessRate(speed) * speed;
+	}
+
+	private double defineSuccessRate(int speed) {
+		if (speed == 0) {
+			return 0.;
+		}
+		if (speed >= 1 && speed <= 4) {
+			return 1.;
+		}
+		if (speed <= 8) {
+			return .9;
+		}
+		if (speed == 9) {
+			return .8;
+		}
+		return .77;
 	}
 
 	public int workingItemsPerMinute(int speed) {
-		throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+		return (int) (productionRatePerHour(speed) / 60);
 	}
 }
