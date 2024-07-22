4182f4ec4047a7fde570ad9ef633e0acfb3d11ea
Roman Dovgan <romadovg@gmail.com>
2024-07-22 15:12:52 +0300

    Solved learning task

--- squeaky-clean/src/main/java/SqueakyClean.java
+++ squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,39 @@
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 class SqueakyClean {
-	static String clean(String identifier) {
-		throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+
+	private static final Map<Character, Character> LEET_MAP = Map.of(
+			'4', 'a',
+			'3', 'e',
+			'0', 'o',
+			'1', 'l',
+			'7', 't'
+	);
+
+	static String clean(String input) {
+		// Step 1: Convert leetspeak to normal text
+		String leetConverted = input.chars()
+				.mapToObj(c -> (char) c)
+				.map(c -> LEET_MAP.getOrDefault(c, c))
+				.map(String::valueOf)
+				.collect(Collectors.joining());
+
+		// Step 2: Convert kebab-case to camelCase
+		String[] parts = leetConverted.split("-");
+		String camelCaseConverted = parts[0] +
+				Stream.of(parts)
+						.skip(1)
+						.map(part -> part.substring(0, 1).toUpperCase() + part.substring(1))
+						.collect(Collectors.joining());
+
+		// Step 3: Omit characters that are not letters
+		String result = camelCaseConverted.chars()
+				.filter(character -> Character.isLetter(character) || Character.isSpaceChar(character))
+				.mapToObj(c -> String.valueOf((char) c))
+				.collect(Collectors.joining());
+
+		return result.replace(' ', '_');
 	}
 }
--- wizards-and-warriors/src/main/java/Fighter.java
+++ wizards-and-warriors/src/main/java/Fighter.java
@@ -9,6 +9,48 @@ class Fighter {
 	}
 }
 
-// TODO: define the Warrior class
+class Warrior extends Fighter {
 
-// TODO: define the Wizard class
+	@Override
+	boolean isVulnerable() {
+		return false;
+	}
+
+	int getDamagePoints(Fighter fighter) {
+		if (fighter.isVulnerable()) {
+			return 10;
+		}
+		return 6;
+	}
+
+	@Override
+	public String toString() {
+		return "Fighter is a Warrior";
+	}
+}
+
+class Wizard extends Fighter {
+
+	private boolean prepared = false;
+
+	public void prepareSpell() {
+		prepared = true;
+	}
+
+	int getDamagePoints(Fighter fighter) {
+		if (prepared) {
+			return 12;
+		}
+		return 3;
+	}
+
+	@Override
+	boolean isVulnerable() {
+		return !prepared;
+	}
+
+	@Override
+	public String toString() {
+		return "Fighter is a Wizard";
+	}
+}
